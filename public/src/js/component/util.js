708b7ccc90e149414d34c907224aafe206460697
jkvim <jkvim@outlook.com>
2015-12-20 17:39:38 +0800

    avatar url 加query

--- public/src/js/component/util.js
+++ public/src/js/component/util.js
@@ -2,8 +2,10 @@ var AVATAR_CDN = 'http://fdn.geekzu.org/avatar/'
 var gravatar = require('gravatar');
 
 function getAvatarByEamil(email, options) {
-	var avatarId = gravatar.url(email, options).match(/.*\/(\w+)?/)[1];
-	return `${AVATAR_CDN}${avatarId}`;
+	var avatarUrl = gravatar.url(email, options);
+	var id = avatarUrl.match(/.*\/([\w\?=]+)/)[1];
+
+	return `${AVATAR_CDN}${id}`;
 }
 
 exports.getAvatarByEamil = getAvatarByEamil;
